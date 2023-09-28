49ec2729ab41dee38063c84fe07f99110fc4351d
5000vw <isaackiplangat1999@gmail.com>
2023-09-28 09:18:45 +0300

    Completed assignment

---
+++ dist/index.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Your code goes here
+document.addEventListener("DOMContentLoaded", function (e) {
+  e.preventDefault();
+  var text = document.querySelector("#text");
+  text.textContent = "This is really cool!";
+});
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(e){
+  e.preventDefault()
+  let text = document.querySelector("#text")
+  text.textContent="This is really cool!"
+})
